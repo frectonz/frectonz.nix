7daec0f4cba426c70e4dc68878bce62a1c89369b
frectonz <fraol0912@gmail.com>
2023-11-12 09:39:15 +0300

    open `.pdf`s. `.epub`s, `.ps`s, `.cbr`s with `zathura`

--- home/zathura.nix
+++ home/zathura.nix
@@ -3,4 +3,11 @@
   programs.zathura = {
     enable = true;
   };
+
+  xdg.mimeApps.defaultApplications = {
+    "application/pdf" = "org.pwmt.zathura.desktop";
+    "application/epub+zip" = "org.pwmt.zathura.desktop";
+    "application/postscript" = "org.pwmt.zathura.desktop";
+    "application/vnd.comicbook-rar" = "org.pwmt.zathura.desktop";
+  };
 }
