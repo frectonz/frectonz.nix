d28f3cafeef4f9a7fa97cfc6de7db5e3c5dccc0d
frectonz <fraol0912@gmail.com>
2023-11-22 15:35:54 +0300

    add nvim plugins

--- home/nvim.nix
+++ home/nvim.nix
@@ -1,9 +1,34 @@
 { config, pkgs, ... }: {
+  home.packages = with pkgs; [ neovide ];
   programs.neovim = {
     enable = true;
 
     viAlias = true;
     vimAlias = true;
     vimdiffAlias = true;
+
+    plugins = with pkgs; [
+      vimPlugins.cmp-git
+      vimPlugins.nvim-cmp
+      vimPlugins.cmp-path
+      vimPlugins.cmp-buffer
+      vimPlugins.cmp-cmdline
+      vimPlugins.cmp-nvim-lsp
+
+      vimPlugins.cmp-vsnip
+      vimPlugins.vim-vsnip
+
+      vimPlugins.nvim-lspconfig
+      vimPlugins.telescope-nvim
+
+      vimPlugins.fidget-nvim
+      vimPlugins.lualine-nvim
+
+      (vimPlugins.nvim-treesitter.withPlugins (p: [
+        p.tree-sitter-nix
+        p.tree-sitter-lua
+        p.tree-sitter-commonlisp
+      ]))
+    ];
   };
 }
