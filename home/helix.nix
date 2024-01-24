1836d76655ec15a4043584f9aad1484d902ea417
frectonz <fraol0912@gmail.com>
2024-01-24 15:13:17 +0300

    comment out deno lsp config

--- home/helix.nix
+++ home/helix.nix
@@ -22,40 +22,40 @@
         indent-guides.render = true;
       };
     };
-    languages = {
-      language = [{
-        name = "typescript";
-        shebangs = [ "deno" ];
-        roots = [ "deno.json" "deno.jsonc" ];
-        file-types = [ "js" "ts" "jsx" "tsx" ];
-        language-servers = [ "deno-lsp" ];
-        auto-format = true;
-      }];
+    # languages = {
+    #   language = [{
+    #     name = "typescript";
+    #     shebangs = [ "deno" ];
+    #     roots = [ "deno.json" "deno.jsonc" ];
+    #     file-types = [ "js" "ts" "jsx" "tsx" ];
+    #     language-servers = [ "deno-lsp" ];
+    #     auto-format = true;
+    #   }];
 
-      language-server.deno-lsp = {
-        command = "deno";
-        args = [ "lsp" ];
-        environment = { NO_COLOR = "1"; };
-        config.deno = {
-          enable = true;
-        };
-      };
+    #   language-server.deno-lsp = {
+    #     command = "deno";
+    #     args = [ "lsp" ];
+    #     environment = { NO_COLOR = "1"; };
+    #     config.deno = {
+    #       enable = true;
+    #     };
+    #   };
 
-      # Uncomment to enable completion of unstable features of Deno
-      # unstable = true
-      # Uncomment to cache dependencies on save
-      # cacheOnSave = true
-      # Enable completion of importing from registries
-      # Enable completion of function calls
-      # suggest = { imports = { hosts = { "https://deno.land" = true, "https://crux.land" = true, "https://x.nest.land" = true } } }
-      # Uncomment to enable inlay hints
-      # inlayHints.parameterNames.enabled = "all"
-      # inlayHints.parameterTypes.enabled = true
-      # inlayHints.variableTypes.enabled = true
-      # inlayHints.propertyDeclarationTypes.enabled  = true
-      # inlayHints.functionLikeReturnTypes.enabled = true
-      # inlayHints.enumMemberValues.enabled = true
-    };
+    #   # Uncomment to enable completion of unstable features of Deno
+    #   # unstable = true
+    #   # Uncomment to cache dependencies on save
+    #   # cacheOnSave = true
+    #   # Enable completion of importing from registries
+    #   # Enable completion of function calls
+    #   # suggest = { imports = { hosts = { "https://deno.land" = true, "https://crux.land" = true, "https://x.nest.land" = true } } }
+    #   # Uncomment to enable inlay hints
+    #   # inlayHints.parameterNames.enabled = "all"
+    #   # inlayHints.parameterTypes.enabled = true
+    #   # inlayHints.variableTypes.enabled = true
+    #   # inlayHints.propertyDeclarationTypes.enabled  = true
+    #   # inlayHints.functionLikeReturnTypes.enabled = true
+    #   # inlayHints.enumMemberValues.enabled = true
+    # };
   };
 
   home.sessionVariables = {
