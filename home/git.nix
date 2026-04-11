4660bab67f0e4fd9eb9dd8f09a8984790a77be05
frectonz <fraol0912@gmail.com>
2026-04-11 22:43:06 +0300

    fix: git user specified in settings

--- home/git.nix
+++ home/git.nix
@@ -5,9 +5,9 @@
 {
   programs.git = {
     enable = true;
-    user.name = "frectonz";
-    user.email = "fraol0912@gmail.com";
     settings = {
+      user.name = "frectonz";
+      user.email = "fraol0912@gmail.com";
       init.defaultBranch = "main";
       user.signing.key = "9CFA458945B7094F";
       commit.gpgSign = true;
